1b3b6252b5645dc5cd9df5cba399539127fb4a66
Timur Mishiev <timurmishiev@gmail.com>
2020-01-07 19:48:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, example)
-  array.insert(4, example)
+def using_insert(array, iphone)
+  array.insert(4, iphone)
 end
 
 def using_uniq(array)
