0b96c1e7bced507a833f9b7e077e0fb75fffe2cd
Timur Mishiev <timurmishiev@gmail.com>
2020-01-07 19:44:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,7 @@ end
 
 def using_unshift(array, string)
   array.unshift(string)
-end 
+end
 
 def using_pop(array)
   array.pop
@@ -24,4 +24,24 @@ end
 
 def using_concat(array1, array2)
   array1.concat(array2)
+end
+
+def using_insert(array, example)
+  array.insert(4, example)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
